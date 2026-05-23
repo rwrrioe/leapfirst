9d73d59c4670fea1aa69a12ac7c02caf4bb68798
rrwrrioe <damir100609@gmail.com>
2026-05-23 20:06:05 +0300

    feat:add initial architecture for config math signal libs

---
+++ core/libs/signal/include/signal/worker.h
@@ -0,0 +1,32 @@
+#pragma once
+#include <functional>
+#include <memory>
+#include <string>
+#include <vector>
+
+class EwmaWorker {
+
+private:
+	struct Impl
+	std::shared_ptr<Impl> impl_;
+};
+
+class ZScoreWorker {
+
+private:
+	struct Impl
+	std::shared_ptr<Impl> impl_;
+};
+
+class BetaWorker {
+private:
+	struct Impl
+	std::shared_ptr<Impl> impl_;
+};
+
+class CorrWorker {
+
+private:
+	struct Impl
+	std::shared_ptr<Impl> impl_;
+};
